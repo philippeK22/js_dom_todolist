f5741e34465bf3d78dd92460b377da1b3144d79f
philippe <kanengelephilippe@gmail.com>
2021-11-10 15:30:36 +0100

    button clear

--- public/js/main.js
+++ public/js/main.js
@@ -1,79 +1,123 @@
-// selecteurs
+// import { deleteCheck } from "./modules/delete.js";
 
+
+// selecteurs
+let formulaire = document.querySelector("form");
+console.log(formulaire);
 let todoInput = document.querySelector(".todo-input");
 console.log(todoInput);
 let todoButton = document.querySelector(".todo-button");
 console.log(todoButton);
 let todoList = document.querySelector(".todo-list");
 console.log(todoList);
+let ulButton = document.querySelector(".todo-list");
+console.log(ulButton);
 let doButton = document.querySelector(".do");
 console.log(doButton);
 let myButton = document.querySelector(".toDo");
 console.log(myButton);
 let buttonAll = document.querySelector(".all");
 console.log(buttonAll);
+let buttonClear = document.querySelector(".clearBtn");
+console.log(buttonClear);
 // ecouteurs evenement
 // todoList.addEventListener("click",deleteCheck);
 
+// formulaires
+
+// formulaire.addEventListener("submit",(e)=>{
+//     const taches = input.value
+//     if (!taches) {
+//         alert('entrez une tache');
+//         return;
+
+        
+//     }
+// })
+
+
+
+// les input
 todoButton.addEventListener("click", (e) => {
     e.preventDefault();
     let todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
+
     // creer mes LI
     let newTodo = document.createElement("li");
     newTodo.innerText = todoInput.value;
     newTodo.classList.add("todo-item");
-    todoDiv.appendChild(newTodo);
-
+    
     // button check
     let completButton = document.createElement("button");
+
     completButton.innerHTML = `<i class="fas fa-check"></i>`
+
     completButton.classList.add("verifie-btn");
-    todoDiv.appendChild(completButton);
+    newTodo.appendChild(completButton);
+    completButton.addEventListener('click', () => {
+        newTodo.style.backgroundColor ="blue"
+        newTodo.classList.toggle("bg-white")
+    })
+    // completButton.addEventListener("dblclick",()=>{
+    //     newTodo.style.backgroundColor =""
+    // })
+    console.log(completButton);
 
     // button save
     let saveButton = document.createElement("button");
     saveButton.innerHTML = `<i class="fas fa-save"></i>`
     saveButton.classList.add("save-btn");
-    todoDiv.appendChild(saveButton);
+    newTodo.appendChild(saveButton);
+    saveButton.addEventListener('click' , () => {
 
+        
+        
+    })
     // button supprimer
     let supprButton = document.createElement("button");
     supprButton.innerHTML = `<i class="far fa-trash-alt"></i>`
     supprButton.classList.add("suppr-btn");
-    todoDiv.appendChild(supprButton);
+    supprButton.addEventListener('click',()=>{
+        ulButton.removeChild(newTodo);
+        
 
-    todoList.appendChild(todoDiv);
+       
+    });
+    newTodo.appendChild(supprButton);
+    todoList.appendChild(newTodo);
+    
+   
+
     todoInput.value = "";
 
+    // les 3 boutons filtrer
 
-})
-
-todoList.addEventListener("click", (e) => {
-    let item = e.target;
-    if (item.classList === "suppr-btn") {
-        let todo = item.parentElement;
-        todoList.classList.add("fall");
-        todo.addEventListener("transitionend", function () {
-
-        });
-    }
-      if (item.classList[0] === "verifie-btn") {
-         todo.classList.toggle();
+     doButton.addEventListener("click",()=>{
+         let newtodoBlue = newTodo.style.backgroundColor="blue"
+        if (newTodo ==  newtodoBlue ) {
+            console.log("okey");
+            
+        }
+     })
+     myButton.addEventListener("click",()=>{
          
-     }
+     })
 
 
+     let divClear = document.querySelector(".clearRouge");
 
-
-    }
-)
-
-todoList.addEventListener("click",(e)=>{
-    let verif = e.target;
-    if(verif.classList ==="verifie-btn"){
+    //  button clear
+    buttonClear.addEventListener("click",()=>{
+        divClear.removeChild(todoList);
         
-    }
-    console.log(verif);
+
+    })
+
+
 
 })
+
+
+
+
---
+++ public/js/modules/delete.js
@@ -0,0 +1,27 @@
+export let deleteCheck =(e) =>{
+   
+    // const item = e.target;
+    
+    
+
+    // // supprimer todo
+    // if (item.classList[0] ==="suppr-btn") {
+    //     const todo = item.parentElement;
+    //     todo.classList.add("fall");
+    //     // todo.addEventListener("transitionend", function () {
+    //     //     todo.remove(newTodo);
+
+    //     // });
+        
+    // }
+    // verifie -btn
+    // if (item.classList[0] === "verifie-btn") {
+      
+    //     const todo = item.parentElement;
+    //     todo.classList.toggle("completed");
+
+        
+    // }
+    
+   
+}
